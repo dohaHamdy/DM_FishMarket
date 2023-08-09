55cc0011956604496d4d5ccc84c9ce9c82396354
Doha Hamdy <dohahamdy94@hotmail.com>
2023-08-09 18:41:19 +0300

    adding the migration

---
+++ webapi/Migration/MigrationExtension.cs
@@ -0,0 +1,34 @@
+﻿namespace webapi.Migration;
+
+using Dapper;
+using FluentMigrator.Runner;
+using System.Data.SqlClient;
+using System.Linq;
+
+public static class MigrationExtension
+{
+    public static void EnsureDatabase(string connectionString, string name)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("name", name);
+        using var connection = new SqlConnection(connectionString);
+        var records = connection.Query("SELECT * FROM sys.databases WHERE name = @name",
+             parameters);
+        if (!records.Any())
+        {
+            connection.Execute($"CREATE DATABASE {name}");
+        }
+    }
+
+    public static IApplicationBuilder Migrate(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var runner = scope.ServiceProvider.GetService<IMigrationRunner>();
+        if (runner!=null)
+        {
+            runner.ListMigrations();
+            runner.MigrateUp();
+        }
+        return app;
+    }
+}
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,11 +1,29 @@
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using webapi.Migration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+      .AddLogging(c => c.AddFluentMigratorConsole())
+      .AddFluentMigratorCore()
+      .ConfigureRunner(c => c
+          .AddSqlServer2012()
+          .WithGlobalConnectionString("Data Source=.;Initial Catalog=MasterFishMarket;Integrated Security=True")
+          .ScanIn(Assembly.GetExecutingAssembly()).For.All()
+          );
+
+
 
 var app = builder.Build();
 
@@ -22,4 +40,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+MigrationExtension.EnsureDatabase(
+        app.Configuration.GetConnectionString("SQLServerConnection")??"",
+        app.Configuration.GetValue<string>("ExtraLocalSetting:MasterDataBase") ?? "");
+
+app.Migrate();
 app.Run();
